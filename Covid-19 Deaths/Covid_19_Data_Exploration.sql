8cf8bb79b6a323fde2d7396781e61d2970311d19
WANJIHIA NJUGUNA <njihiat254@gmail.com>
2023-08-12 23:34:19 +0300

    Update Covid_19_Data_Exploration.sql
    Refined the Code and Comments

--- Covid-19 Deaths/Covid_19_Data_Exploration.sql
+++ Covid-19 Deaths/Covid_19_Data_Exploration.sql
@@ -1,44 +1,57 @@
---This code returns the data ordered by columns 3 and 4
+/*
+Covid 19 Data Exploration 
+
+Skills used: 
+1. Converting Data types using CAST() function
+2. Performing Calculations on the data
+3. Aggregate functions
+
+In this Exploration, I used cast often to change the data type from nvarchar to those types that calculations won't run into an error
+*/
+
+--Querying all the data ordered by columns 3 and 4. The where clause excludes the data on the world.
 select *
 from PortfolioProject..CovidDeaths
-order by 3,4
+Where continent is not NULL
+Order By 3,4
 
---select *
---from PortfolioProject..CovidVacinnations
---order by 3,4
-
---Select Data that we are going to be using
-
+--Select the Data that we are going to be using. This excludes the data on other columns that aren't in focus.
 Select location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
 Order by 1,2
 
--- Looking at Total Cases and Total Deaths in kenya ordered by columns 1 and then 2
-
-Select location, date, total_cases, total_deaths, 
+/*Looking at Total Cases, Total Deaths, and the date they were recorded in Kenya ordered by columns 1 and then 2
+	This query utilizes the CAST function to convert columns that are nvarchar to DECIMAL for calculations 
+	The query shows a DeathPercentage of 1.654 as of 26th July 2023
+*/
+Select location, date, total_cases, total_deaths, (CAST(total_deaths AS DECIMAL)/CAST(total_cases AS DECIMAL)) as DeathPercentage
 From PortfolioProject..CovidDeaths
 Where location like '%Kenya%'
 Order by 1,2
 
---looking at Total Cases vs population
---- Shows what percentge of population got Covid
-
-Select location, date, total_cases, population, (total_cases/population)*100 as percentange_pop_affected
+--looking at Total Cases vs population. This shows a 0.636% of the population affected as of 26th July 2023
+Select location, date, total_cases, population, (CAST(total_cases AS FLOAT)/population)*100 as percentange_pop_affected
 From PortfolioProject..CovidDeaths
 Where location like '%Kenya%'
 Order by 1,2
 
--- Looking at Countries with Highest Infection Rate Compared to Population
-
-Select location, population, MAX(total_cases) as highest_infection_count, Max((total_cases/population))*100 as percent_pop_infected
+/*
+Looking at Countries with Highest Infection Rate Compared to Population.
+	In this query, CAST() transformed the total cases from nvarchar to INT. Using MAX() returned 99999 as the 
+	highest total cases, nvarchar values are sorted in lexicographic order. By casting, I effectively converted the string
+	representation of the numbers to actual numerical values
+*/	
+Select location, population, MAX(CAST(total_cases as INT) as highest_infection_count, Max((total_cases/population))*100 as percent_pop_infected
 From PortfolioProject..CovidDeaths
 --Where location like '%Kenya%'
 Group by location, population
 Order by percent_pop_infected desc
 
--- showing countries with highest death count per population
-
-Select location, MAX(cast(total_deaths as int)) as highest_death_count
+/*
+	Looking at the countries with their highest death count. This query shows that USA recorded the highest death count
+	as of 26th July 2023 with 1127152 people succumbing to Covid-19.
+*/
+Select location, MAX(CAST(total_deaths AS int)) AS highest_death_count
 From PortfolioProject..CovidDeaths
 --Where location like '%Kenya%'
 where continent is not null
@@ -47,8 +60,16 @@ Order by highest_death_count desc
 
 
 --summarizing things by continent
--- Looking at continent with highest death count
-
+/*
+Looking at the continent with the highest death count. Turns out North America leads followed closely by South America.
+	The query results are as follows:
+	North America	1127152
+	South America	704488
+	Asia		531915
+	Europe		399814
+	Africa		102595
+	Oceania		22308
+*/
 Select continent, MAX(cast(total_deaths as int)) as highest_death_count
 From PortfolioProject..CovidDeaths
 --Where location like '%Kenya%'
@@ -56,16 +77,7 @@ where continent is not null
 Group by continent
 Order by highest_death_count desc
 
---Global Numbers
-
-Select date, SUM(new_cases), SUM(cast(new_deaths as int)), SUM(cast(new_deaths as int))/SUM(cast(new_cases as int))*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
-where continent is not null
-Group By date
-Order By 1,2
-
---Looking at total population vs vaccinations
-
+--Looking at total population vs new vaccinations. This query utilizes join on another table called Vaccinations
 Select dea.continent, dea.location, dea.date, dea.population,
 	vac.new_vaccinations,
 From PortfolioProject..CovidDeaths dea
@@ -80,7 +92,7 @@ SELECT total_deaths
 FROM PortfolioProject..CovidDeaths
 WHERE ISNUMERIC(total_deaths) = 0;
 
---this code ensure that Null variables are excluded and only not null values are considered in the calculation.
+--this code ensures that Null variables are excluded and only not null values are considered in the calculation.
 SELECT MAX(total_deaths)
 FROM PortfolioProject..CovidDeaths
 WHERE location = 'World' AND total_deaths IS NOT NULL;
